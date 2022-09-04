13d61e39f9ef1ab4506d570b6dc63248b6af2991
nitish4757 <nitish4757@gmail.com>
2022-09-04 14:03:51 +0530

    testing git changes

--- src/patterns/factory/actual/Main.java
+++ src/patterns/factory/actual/Main.java
@@ -7,9 +7,6 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-
-
-
         Factory factory = new Factory();
         var plan = factory.getPlanObject("COMMERCIAL");
         plan.getRate();
